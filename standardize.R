95361fe217d26d2e28158c3bc70d69678d7558a6
Dark Claw <leo0966472779@gmail.com>
2026-05-25 10:39:54 +0800

    對變數做標準化

---
+++ standardize.R
@@ -0,0 +1,12 @@
+
+# 1. 拆分資料：只把 X 特徵拿出來做標準化，排除第 12 欄的 quality (Y)
+X_scaled <- scale(wine_analysis[, -12])
+
+# 🌟 2. 關鍵修正：因為 scale() 會把資料變成 matrix，我們要立刻將它轉回 data.frame
+X_scaled <- as.data.frame(X_scaled)
+
+# 3. 把完全沒被破壞、維持原樣的 quality (Y) 加回去
+wine_ready <- X_scaled
+wine_ready$quality <- wine_analysis$quality
+
+str(wine_ready)
