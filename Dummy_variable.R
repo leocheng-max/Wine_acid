ef8db104c71ee7cdd18cd989a2ba75fc48dcc039
Dark Claw <leo0966472779@gmail.com>
2026-05-26 09:29:12 +0800

    建立虛擬變數dummy variable[0,1]]

---
+++ Dummy_variable.R
@@ -0,0 +1,15 @@
+
+
+# >=6 設為1,<6設為0
+wine_analysis$good_wine <- ifelse(wine$quality >= 6, 1, 0)
+
+#  將目標變數轉換為 factor
+wine_analysis$good_wine <- factor(wine_analysis$good_wine, levels = c(0, 1), labels = c("Bad", "Good"))
+
+# 4. 移除原本的 quality 欄位，避免模型偷看答案
+wine_analysis <- wine_analysis[, !names(wine) %in% c("quality")]
+
+# 查看轉換後的類別分布
+table(wine_analysis$good_wine)
+
+summary(wine_analysis)
